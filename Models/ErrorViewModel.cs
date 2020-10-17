affccd4bfa4f70363da8fc3de0d2261071273db5
Walter Arce <walterarce@gmail.com>
2020-10-17 16:15:31 -0300

    Se sube primer version solo archivos base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcRazorWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
